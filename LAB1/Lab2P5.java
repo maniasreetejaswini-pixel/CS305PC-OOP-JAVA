1707a8d3501e883b1a20f9e13eec9c9812c2909d
maniasreetejaswini-pixel <maniasreetejaswini@gmail.com>
2025-09-10 12:30:47 +0530

    Add files via upload

---
+++ LAB1/Lab2P5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Lab2P5{
+	public static void main(String...args){
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int count = 0;
+		
+		int i = 2;
+		while(i<0){
+		
+		}
+		System.out.println("is a prime number");
+	}
+}
