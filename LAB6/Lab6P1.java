1a0cd294d3f2dd3cb0995fa70492d6101efc3e3f
maniasreetejaswini-pixel <maniasreetejaswini@gmail.com>
2025-10-29 10:39:13 +0530

    Add files via upload

---
+++ LAB6/Lab6P1.java
@@ -0,0 +1,14 @@
+enum Fruits{
+	Apple,Banana,Citrus,Dragonfruit,Fig,Grapes;
+}
+public class Lab6P1{
+	public static void main(String...args){
+		for(Fruits f: Fruits.values()){
+		System.out.println(f);
+		}
+	}
+}
+
+
+
+
