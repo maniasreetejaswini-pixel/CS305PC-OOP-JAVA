f416b80dd5b25577138a3b52e8cad54f497a386a
maniasreetejaswini-pixel <maniasreetejaswini@gmail.com>
2025-11-26 10:52:29 +0530

    Add files via upload

---
+++ LAB3/Lab3P61.java
@@ -0,0 +1,13 @@
+class Lab3P61{
+	String name;
+	public void demonstrate(){
+		System.out.println("I am a method in Lab3P61");
+	}
+	public void setName(String s){
+		this.name=s;
+	}
+	public String getName(){
+		return this.name;
+	}
+}		
+			
